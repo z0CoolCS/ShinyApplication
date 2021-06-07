6497118e3be73c5a567211712f64d6ab62436e57
GioCS <Gio@>
2021-06-06 20:32:21 -0500

    presentation

--- Project_DataScience/server.R
+++ Project_DataScience/server.R
@@ -82,8 +82,8 @@ shinyServer(function(input, output) {
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
         
         hist(x, breaks = bins, col = "#75AADB", border = "white",
-             xlab = "Waiting time to next eruption (in mins)",
-             main = "Histogram of waiting times")
+             xlab = "Education Level",
+             main = "Percentage of people with education beyond primary")
         
     })
 
